bf7b70c9ebb1ba8e2d476a5ad44a90707755cc76
robinmalgoire <malgoire.robin@gmail.com>
2020-12-05 22:31:42 +1100

    chore: add linaria

--- next.config.js
+++ next.config.js
@@ -6,7 +6,27 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 const withPreact = require('next-plugin-preact');
 
+const withCSS = require("@zeit/next-css");
+
 module.exports = withPlugins([
   [withBundleAnalyzer],
+  [withCSS],
   [withPreact]
-])
+], { 
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.tsx$/,
+      use: [
+        {
+          loader: 'linaria/loader',
+          options: {
+            sourceMap: process.env.NODE_ENV !== 'production',
+          },
+        },
+      ],
+    })
+
+    return config
+  },
+})
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,29 @@
-import Link from 'next/link'
+import Link from "next/link";
+import { styled } from "linaria/react";
+
+const Box = styled.div`
+  margin-top: 40px;
+  margin-left: 40px;
+  height: 200px;
+  width: 200px;
+  background-color: tomato;
+  animation: spin 2s linear infinite;
+
+  @keyframes spin {
+    from {
+      transform: rotate(0deg);
+    }
+
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
 
 export default function Home() {
   return (
     <div>
-      Hello World.{' '}
+      Hello World.{" "}
       <ul>
         <li>
           <Link href="/about">
@@ -21,6 +41,7 @@ export default function Home() {
           </Link>
         </li>
       </ul>
+      <Box>test</Box>
     </div>
-  )
+  );
 }
