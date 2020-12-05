8147ceed2932a61e05b9a029fa320247c278b0d8
robinmalgoire <malgoire.robin@gmail.com>
2020-12-05 22:50:50 +1100

    chore: move pages and common into src folder

---
+++ src/common/Box.tsx
@@ -0,0 +1,9 @@
+import { styled } from "linaria/react";
+
+const Box = styled.div`
+  padding: 40px;
+  margin: 20px;
+  background-color: tomato;
+`;
+
+export default Box;
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { FunctionComponent as FC } from "preact";
+
+import Box from "common/Box";
+
+const Index: FC<{ ssg: string }> = ({ ssg }) => (
+  <div>
+    <Box>{ssg}</Box>
+  </div>
+);
+
+export const getStaticProps = () => ({ props: { ssg: "static props" } });
+
+export default Index;
