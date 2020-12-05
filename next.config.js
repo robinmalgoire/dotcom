422bbd0fb29f00890cdcd3d2b57cfac17a18e237
robinmalgoire <malgoire.robin@gmail.com>
2020-12-05 21:11:58 +1100

    chore: add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
-const withPreact = require('next-plugin-preact')
+const withPlugins = require('next-compose-plugins');
 
-module.exports = withPreact({
-  /* regular next.js config options here */
-})
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+const withPreact = require('next-plugin-preact');
+
+module.exports = withPlugins([
+  [withBundleAnalyzer],
+  [withPreact]
+])
